8f109608a20c6483b9f0a8fabf2a3590ddb270ca
hugeMordor <72040616+hugeMordor@users.noreply.github.com>
2021-05-19 14:02:33 +0300

    Add files via upload

---
+++ UserControl1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Lib1
+{
+    partial class UserControl1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(62, 26);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(289, 193);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(62, 223);
+            this.trackBar1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.trackBar1.Minimum = -10;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(289, 45);
+            this.trackBar1.TabIndex = 1;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.Both;
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(356, 26);
+            this.trackBar2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.trackBar2.Minimum = -10;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar2.Size = new System.Drawing.Size(45, 193);
+            this.trackBar2.TabIndex = 3;
+            this.trackBar2.TickStyle = System.Windows.Forms.TickStyle.Both;
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MinimumSize = new System.Drawing.Size(45, 49);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(674, 446);
+            this.Load += new System.EventHandler(this.UserControl1_Load);
+            this.Resize += new System.EventHandler(this.UserControl1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+    }
+}
---
+++ UserControl1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lib1
+{
+    public partial class UserControl1: UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+        Bitmap BM;
+        Graphics Gr;
+        int CellX=1, CellY=1;
+        bool flagCell=false;
+        Pen penCell;
+
+
+        bool FlagGraph = false;
+        Pen PenGraph;
+        int degree; double[] Coeff;
+        double XminBase = -1, XmaxBase = 1, YminBase = -1, YmaxBase = 1;
+        double Xmin, Xmax, Ymin, Ymax;
+        double Kx, Ky;
+        double x, y;
+        double XXX;
+        int X1scr, Y1scr, X2scr, Y2scr;
+       
+        private void UserControl1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Left = 0;
+            pictureBox1.Top = 0;
+            pictureBox1.Width = this.Width - 50;
+            pictureBox1.Height = this.Height - 50;
+
+
+            trackBar1.Left = 0;
+            trackBar1.Top = pictureBox1.Height + 5;
+            trackBar1.Width = pictureBox1.Width;
+
+
+            trackBar2.Left = pictureBox1.Width + 5;
+            trackBar2.Top = 0;
+            trackBar2.Height = pictureBox1.Height;
+        }
+
+        private void UserControl1_Resize(object sender, EventArgs e)
+        {
+            pictureBox1.Left = 0;
+            pictureBox1.Top = 0;
+            pictureBox1.Width = this.Width - 50;
+            pictureBox1.Height = this.Height - 50;
+
+
+            trackBar1.Left = 0;
+            trackBar1.Top = pictureBox1.Height + 5;
+            trackBar1.Width = pictureBox1.Width;
+
+
+            trackBar2.Left = pictureBox1.Width + 5;
+            trackBar2.Top = 0;
+            trackBar2.Height = pictureBox1.Height;
+
+        }
+
+        public void InitGraph()
+        {
+            BM = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Gr = Graphics.FromImage(BM);
+        }
+
+        private void ReDrawPicture()
+        {
+            Gr.Clear(pictureBox1.BackColor);
+
+            if(flagCell)
+            {
+                for (int i = 0; i <= CellX; i++) Gr.DrawLine(penCell, (int)(i * BM.Width/CellX), 0,(int)(i*BM.Width/CellX),BM.Height);/* */
+
+                for (int i = 0; i <= CellY; i++) Gr.DrawLine(penCell, 0, (int)(i * BM.Height / CellY), BM.Width, (int)(i * BM.Height / CellY));
+            }
+
+            pictureBox1.Image = BM;
+        }
+
+        public void SetBackColor(Color cl)
+        {
+            pictureBox1.BackColor = cl;
+        }
+
+        public void AddCell(int Nx,int Ny,Color Cl,int w)
+        {
+            flagCell = true;
+            CellX = Nx;
+            CellY = Ny;
+            penCell = new Pen(Cl,w);
+            ReDrawPicture();
+        }
+
+        public void DelCell()
+        {
+            flagCell = false;
+            penCell.Dispose();
+            ReDrawPicture();
+        }
+        public void AddGraph(int N, double[] A, Color cl, int W)
+        {
+            degree = N;
+            Coeff = new double[N + 1];
+            for (int i = 0; i <= N; i++)
+            {
+                Coeff[i] = A[i];
+            }
+        }
+        public void DeleteGraph()
+        {
+
+        }
+
+
+
+
+
+
+    }
+}
