d3dbd263b9e673c3bce9a76d0d017a919479e37e
hugeMordor <72040616+hugeMordor@users.noreply.github.com>
2021-05-24 21:25:52 +0300

    Add files via upload

--- UserControl1.Designer.cs
+++ UserControl1.Designer.cs
@@ -31,6 +31,8 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.trackBar1 = new System.Windows.Forms.TrackBar();
             this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
@@ -40,42 +42,71 @@
             // 
             this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
             this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Cross;
             this.pictureBox1.Location = new System.Drawing.Point(62, 26);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(289, 193);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseEnter += new System.EventHandler(this.pictureBox1_MouseEnter);
+            this.pictureBox1.MouseLeave += new System.EventHandler(this.pictureBox1_MouseLeave);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseWheel += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseWheel);
             // 
             // trackBar1
             // 
             this.trackBar1.Location = new System.Drawing.Point(62, 223);
-            this.trackBar1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.trackBar1.Margin = new System.Windows.Forms.Padding(2);
             this.trackBar1.Minimum = -10;
             this.trackBar1.Name = "trackBar1";
             this.trackBar1.Size = new System.Drawing.Size(289, 45);
             this.trackBar1.TabIndex = 1;
             this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
             // 
             // trackBar2
             // 
             this.trackBar2.Location = new System.Drawing.Point(356, 26);
-            this.trackBar2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.trackBar2.Margin = new System.Windows.Forms.Padding(2);
             this.trackBar2.Minimum = -10;
             this.trackBar2.Name = "trackBar2";
             this.trackBar2.Orientation = System.Windows.Forms.Orientation.Vertical;
             this.trackBar2.Size = new System.Drawing.Size(45, 193);
             this.trackBar2.TabIndex = 3;
             this.trackBar2.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(374, 234);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(27, 23);
+            this.button1.TabIndex = 4;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(71, 283);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
             // 
             // UserControl1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.trackBar2);
             this.Controls.Add(this.trackBar1);
             this.Controls.Add(this.pictureBox1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.MinimumSize = new System.Drawing.Size(45, 49);
             this.Name = "UserControl1";
             this.Size = new System.Drawing.Size(674, 446);
@@ -94,5 +125,7 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.TrackBar trackBar1;
         private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
--- UserControl1.cs
+++ UserControl1.cs
@@ -7,11 +7,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Input;
+
+using static System.Math;
+
 
 namespace Lib1
 {
     public partial class UserControl1: UserControl
     {
+        
         public UserControl1()
         {
             InitializeComponent();
@@ -26,13 +31,110 @@ namespace Lib1
         bool FlagGraph = false;
         Pen PenGraph;
         int degree; double[] Coeff;
-        double XminBase = -1, XmaxBase = 1, YminBase = -1, YmaxBase = 1;
+        double XminBase = -1, XmaxBase = 1, YminBase = -1, YmaxBase = 1, Xmouse = 0, Ymouse = 0;
+        double Xm1 = 0, Xm2 = 0, Ym1 = 0, Ym2 = 0;
         double Xmin, Xmax, Ymin, Ymax;
         double Kx, Ky;
         double x, y;
         double XXX;
         int X1scr, Y1scr, X2scr, Y2scr;
-       
+        double Yscr;
+        double K = 1;
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            trackBar1.Value = 0;
+            trackBar2.Value = 0;
+            K = 1;
+            ReDrawPicture();
+        }
+
+        public void SetBaseRectangle(double X, double Y, double L, double H)
+        {
+            XminBase = X;
+            YminBase = Y - H;
+            XmaxBase = X + L;
+            YmaxBase = Y;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Xm2 = e.X; Ym2 = e.Y;
+            label1.Left = e.X + 10;
+            label1.Top = e.Y + 10;
+            x = (Xmin + e.X) / Kx;
+            y = (-e.Y + BM.Height - Ky * Ymin) / Ky;
+            //Yscr = BM.Height-Ky*(y-Ymin)
+            //y*Ky = -Yscr + BM.H - Ky*Ymin
+            label1.Text = String.Format("( {0:f2}, {1:f2})", x, y);
+            if (e.Button == MouseButtons.Left)
+            {
+
+                //if (Xm1 < Xm2)
+                //{
+                //    Xmouse += -(pictureBox1.Left-e.X)*K / Kx/250;
+                //}
+                //if (Xm1 > Xm2)
+                //{
+                //    Xmouse += (pictureBox1.Left-e.X)*K / Kx/250;
+                //}
+                //if (Ym1 < Ym2)
+                //{
+                //    Ymouse += -(pictureBox1.Top-e.Y)*K / Ky/250;
+                //}
+                //if (Ym1 > Ym2)
+                //{
+                //    Ymouse += (pictureBox1.Top-e.Y)*K / Ky/250;
+                //}
+                Xmouse += Xm2 - Xm1;
+                Ymouse += Ym2 - Ym1;
+                
+
+                ReDrawPicture();
+            }
+            
+            Xm1 = e.X; Ym1 = e.Y;
+        }
+        //private void pictureBox1_MouseButtonMove(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        //{
+
+        //}
+
+        private void pictureBox1_MouseEnter(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+        }
+
+        private void pictureBox1_MouseLeave(object sender, EventArgs e)
+        {
+            
+            label1.Visible = false;
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_MouseWheel(object sender, MouseEventArgs e)
+        {
+            K *= Pow(1.1, e.Delta/100);
+            ReDrawPicture();
+        }
+
+        
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            ReDrawPicture();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            ReDrawPicture();
+        }
+
         private void UserControl1_Load(object sender, EventArgs e)
         {
             pictureBox1.Left = 0;
@@ -49,6 +151,10 @@ namespace Lib1
             trackBar2.Left = pictureBox1.Width + 5;
             trackBar2.Top = 0;
             trackBar2.Height = pictureBox1.Height;
+
+            button1.Left = pictureBox1.Width + 5;
+            button1.Top = pictureBox1.Height + 5;
+            button1.Width = 40; button1.Height = 40;
         }
 
         private void UserControl1_Resize(object sender, EventArgs e)
@@ -68,6 +174,9 @@ namespace Lib1
             trackBar2.Top = 0;
             trackBar2.Height = pictureBox1.Height;
 
+            button1.Left = pictureBox1.Width + 5;
+            button1.Top = pictureBox1.Height + 5;
+            button1.Width = 40;button1.Height = 40;
         }
 
         public void InitGraph()
@@ -82,10 +191,69 @@ namespace Lib1
 
             if(flagCell)
             {
-                for (int i = 0; i <= CellX; i++) Gr.DrawLine(penCell, (int)(i * BM.Width/CellX), 0,(int)(i*BM.Width/CellX),BM.Height);/* */
+                for (int i = 0; i <= CellX; i++) Gr.DrawLine(penCell, (int)(i * BM.Width/CellX), 0,(int)(i*BM.Width/CellX),BM.Height);
 
                 for (int i = 0; i <= CellY; i++) Gr.DrawLine(penCell, 0, (int)(i * BM.Height / CellY), BM.Width, (int)(i * BM.Height / CellY));
             }
+
+            if (FlagGraph)
+            {
+                //Xscr = Kx*(x-Xmin)
+                //Yscr = BM.Height-Ky*(y-Ymin)
+                Xmin = (XminBase + XmaxBase) / 2 - ((XmaxBase - XminBase) / 2) * K*Pow(1.1, trackBar1.Value);
+                Xmax = (XminBase + XmaxBase) / 2 + ((XmaxBase - XminBase) / 2) * K*Pow(1.1, trackBar1.Value);
+                Ymin = (YminBase + YmaxBase) / 2 - ((YmaxBase - YminBase) / 2) * K*Pow(1.1, trackBar2.Value);
+                Ymax = (YminBase + YmaxBase) / 2 + ((YmaxBase - YminBase) / 2) * K*Pow(1.1, trackBar2.Value);
+                Kx = BM.Width / (Xmax - Xmin);
+                Ky = BM.Height / (Ymax - Ymin);
+
+                X1scr = 0;
+                x = Xmin;
+                XXX = 1;
+                y = 0;
+                for (int i = degree; i >= 0; i--)
+                {
+                    y += Coeff[i] * XXX;
+                    XXX *= x;
+                }
+                
+                Y1scr = (int)(BM.Height - Ky * (y - Ymin));
+                //if (Yscr < 0) Y1scr = -1;
+                //else Y1scr = (int)Yscr;
+
+                //if (Yscr > BM.Height) Y1scr = BM.Height + 1;
+                //else Y1scr = (int)Yscr;
+
+                for (int j = 1; j <= BM.Width; j++)
+                {
+                    X2scr = j;
+                    x = Xmin + X2scr / Kx;
+                    XXX = 1;y = 0;
+                    for (int i = degree; i >= 0; i--)
+                    {
+                        y += Coeff[i] * XXX;
+                        XXX *= x;
+                    }
+                    Y2scr = (int)(BM.Height - Ky * (y - Ymin));
+                    //if (Yscr < 0) Y2scr = -1;
+                    //else Y2scr = (int)Yscr;
+                    
+                    //if (Yscr > BM.Height) Y2scr = BM.Height + 1;
+                    //else Y2scr = (int)Yscr;
+
+                    try
+                    {
+                        Gr.DrawLine(PenGraph, (int)(X1scr+Xmouse), (int)(Y1scr + Ymouse), (int)(X2scr + Xmouse), (int)(Y2scr + Ymouse));
+                    }
+                    catch (System.OverflowException) {
+                        X1scr = X2scr;
+                        Y1scr = Y2scr;
+                    }
+                    X1scr = X2scr;
+                    Y1scr = Y2scr;
+                }
+            }
+            
 
             pictureBox1.Image = BM;
         }
@@ -118,10 +286,16 @@ namespace Lib1
             {
                 Coeff[i] = A[i];
             }
+            PenGraph = new Pen(cl, W);
+            FlagGraph = true;
+            ReDrawPicture();
         }
         public void DeleteGraph()
         {
-
+            FlagGraph = false;
+            PenGraph.Dispose();
+            Array.Clear(Coeff, 0, degree+1);
+            ReDrawPicture();
         }
 
 
